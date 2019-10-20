9c7d80fc06108783a133415bcb3c98cdfca82b02
zocom-kim-vagi <54845585+zocom-kim-vagi@users.noreply.github.com>
2019-10-20 20:53:41 +0200

    Lägger upp inför måndag

---
+++ Exercice1-Array/scripts.js
@@ -0,0 +1,60 @@
+/*
+  1. Skriv koden för att sätta ett av värdena
+  så att numrena i arrayen nedanför är i följd.
+*/
+var list = [1, 2, 3, 7, 5, 6, 7, 8, 9];
+
+/*
+  2. Använd den arrayspecifika funktionen som lägger 
+  till data i slutet för att lägga till korrekt nummer i arrayen
+*/
+
+var list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+/*
+  3. Skapa en array med namnet values som har följande innehålla
+  i rätt ordning:
+
+  1. Ett nummer
+  2. En sträng
+  3. En boolean
+  
+*/
+
+
+
+/*
+4. Använd din nya array och den array-metod som
+tar bort det sista värdet i arrayen
+och returnerar det. Spara det som returneras
+i en variabel som heter bool.
+*/
+
+
+
+
+/*
+5. Kolla på koden nedanför. eightiesMovies är en så kallad
+tvådimensionell array, två arrayer i en array!
+Skapa en variabel med namnet infant och ta ut värdet
+baby ur den tvådimensionella array genom att peka på rätt
+index.
+
+*/
+
+var movie1 = [16, "Candles"];
+var movie2 = [3, "Men", "and", "a", "Baby"];
+var eightiesMovies = [movie1, movie2];
+
+
+/*
+6. Använd en alert och skicka ut namnet på första filmen, 
+men endast genom att använda eightiesMovies.
+I denna uppgift, använd concatenation för att sätta
+ihop orden till en sträng.
+*/
+
+var movie1 = [16, "Candles"];
+var movie2 = [3, "Men", "and", "a", "Baby"];
+var eightiesMovies = [movie1, movie2];
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# JavaScript-Vecka43
-Veckoplan, Vad vi ska göra
+﻿Denna vecka kommer vi att lära oss mer om 
+
+Repetition
+
+- Lite om Best practice  
+
+- Operator Modulus
+
+- Functioner
+
+- Higher order Functions
+
+Nytt men som vi börjat titta på 
+
+- Array.methods() 
+
+- Objekt -
+
+Nytt
+
+- Object Date()
+
+- RegExp Object (Regular Expression)
+
+- loopar  Switch, for, while
+
+- LocalStorage / webstorage / Coocies
+
+- Location / History 
