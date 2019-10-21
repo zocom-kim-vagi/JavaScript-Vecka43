10ff139851a51c3411a5f94e44c08b2fef7de59d
zocom-kim-vagi <54845585+zocom-kim-vagi@users.noreply.github.com>
2019-10-21 12:26:10 +0200

    Lägger upp övning med time

---
+++ exercice3-Klocka/datetime.js
@@ -0,0 +1,17 @@
+/*
+Gör en klocka som visar i HTML dokumentet
+21 oktober 2019
+Klockan är: tt.mm.ss
+
+Läs på om JavaScript Time på 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+
+
+För er som ligger rätt långt fram, gör en timer som räknar ner dagar  timmar, minuter och sekunder 
+fram till Nyår, en S.k. Countdown
+
+Hur ni kan räkna hittar ni på https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/now
+Tips: använd "setInterval(function() { } " istället för setTimeout(function() { }
+
+
+*/
